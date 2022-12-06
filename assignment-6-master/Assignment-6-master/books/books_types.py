394706a1f9f225a9438c74790f902421c5447e15
EbruCev <59918079+EbruCev@users.noreply.github.com>
2022-12-06 11:20:58 -0500

    task 1 done

--- assignment-6-master/Assignment-6-master/books/books_types.py
+++ assignment-6-master/Assignment-6-master/books/books_types.py
@@ -12,3 +12,20 @@ import statistics
 
 # TODO: Add your code to tasks 1 to 5 in this file.
 
+class Book:
+    def __init__(self, title, author, rating, reviews, price, years, genre):
+        self.title = title
+        self.author = author
+        self.rating = rating
+        self.reviews = reviews
+        self.price = price
+        self.years = years
+        self.genre = genre
+
+    def __str__(self):
+        return self.title
+
+
+class Amazon:
+    def __init__(self, bestsellers):
+        self.bestsellers = bestsellers
