69eeb61e6018699f50e147dfdb02bdd044deb657
EbruCev <59918079+EbruCev@users.noreply.github.com>
2022-12-07 18:24:09 -0500

    task 3 completed

--- assignment-6-master/Assignment-6-master/books/books_types.py
+++ assignment-6-master/Assignment-6-master/books/books_types.py
@@ -13,7 +13,7 @@ import statistics
 # TODO: Add your code to tasks 1 to 5 in this file.
 
 class Book:
-    def __init__(self, title, author, rating, reviews, price, years, genre):
+    def __init__(self, title, author, rating, reviews, price, years, genre = ""):
         self.title = title
         self.author = author
         self.rating = rating
@@ -21,18 +21,19 @@ class Book:
         self.price = price
         self.years = years
         self.genre = genre
+        self.FICTION = 'Fiction'
+        self.NON_FICTION = 'Non fiction'
 
     def __str__(self):
         return self.title
 
 
-class Amazon:
-    def __init__(self, bestsellers):
-        self.bestsellers = bestsellers
+
 
 class FictionBook(Book):
     def __init__(self, title, author, rating, reviews, price, years):
-        super().__init__(title, author, rating, reviews, price, years, self.FICTION)
+        super().__init__(title, author, rating, reviews, price, years)
+        self.genre = self.FICTION
 
     def __str__(self):
         output = ""
@@ -47,7 +48,8 @@ class FictionBook(Book):
 
 class NonFictionBook(Book):
     def __init__(self, title, author, rating, reviews, price, years):
-        super().__init__(title, author, rating, reviews, price, years, self.NON_FICTION)
+        super().__init__(title, author, rating, reviews, price, years)
+        self.genre = self.NON_FICTION
 
     def __str__(self):
         output = ""
@@ -58,3 +60,24 @@ class NonFictionBook(Book):
             year_list = year + separator
         output = self.title + ":" + self.genre + "(" + year_list + ")"
         return output
+
+
+class Amazon:
+    def __init__(self, bestsellers):
+        self.bestsellers = bestsellers
+
+    def read_books_csv(self, path):
+        with open(path, 'r', encoding='utf-8-sig') as csvdata:
+            csvreader = csv.reader(csvdata)
+            data_list = list(csvreader)
+        books_dict = {}
+        for row in data_list[1:]:
+            if row[6] == "Fiction":
+                book = FictionBook(row[0], row[1], float(row[2]), int(row[3]), int(row[4]), [int(row[5])])
+            else:
+                book = NonFictionBook(row[0], row[1], float(row[2]), int(row[3]), int(row[4]), [int(row[5])])
+            if row[0] in books_dict:
+                books_dict[row[0]].years.append(int(row[5]))
+            else:
+                books_dict.update({row[0]: book})
+        self.bestsellers = list(books_dict.values())
